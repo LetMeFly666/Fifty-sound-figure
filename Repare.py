b58a8b651890b3894767af12ff4f7cc656d494f2
LetMeFly666 <814114971@qq.com>
2022-02-10 00:20:41 +0800

    1. 使用发现有一个无法访问和两个404，因此修改所有页面的js、css为项目中的文件，取消了对第三方文件的访问。 2. 修改了布局，包括取消3个div中class的container，否则单个div就会占据100%的width、修改.riyu-content的height，(此处没有)仔细调，但是使得屏幕不会因容器过高而出现点击时闪烁、 3. 增加了Repare.py(除riyu.html外，其他网页均由此脚本修改)

---
+++ Repare.py
@@ -0,0 +1,51 @@
+'''
+Author: LetMeFly
+Date: 2022-02-09 11:57:55
+LastEditors: LetMeFly
+LastEditTime: 2022-02-10 00:14:33
+'''
+import os
+
+# def dfs(dir_now):
+#     for i in os.listdir(dir_now):
+#         if os.path.isdir(i):
+#             print("dir:", i)
+#             for j in dfs(i):
+#                 yield os.path.join(i, j)
+#         else:
+#             yield os.path.abspath(i)
+
+def dfs(dir_now):
+    ans = []
+    for root, dirs, files in os.walk(dir_now):
+        for file in files:
+            ans.append(os.path.join(root, file))
+    return ans
+
+
+if __name__ == "__main__":
+    to_replace = [
+        ('https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js', '"../../../js/jquery-3.6.0.min.js"'),
+        ('https://cdn.bootcss.com/bootstrap/4.1.0/css/bootstrap.min.css', '"../../../css/bootstrap.min.css"'),
+        ('https://cdn.bootcss.com/bootstrap/4.1.0/js/bootstrap.min.js', '"../../../js/bootstrap.min.js"'),
+        ('https://cdn.bootcss.com/popper.js/1.14.0/umd/popper.min.js', '"../../../js/popper.min.js"'),
+        ('https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css', '"../../../css/APlayer.min.css"'),
+        ('https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js', '"../../../js/APlayer.min.js"'),
+        ('riyu-content-yintu tab-pane container', 'riyu-content-yintu tab-pane'),
+        ('tab-pane container active', 'tab-pane active')
+    ]
+
+
+    files = dfs(os.getcwd())
+    for file in files:
+        if not file.endswith(".html"):
+            continue
+        print(file)
+        with open(file, "r", encoding='utf-8') as f:
+            content = f.read()
+        for this_to_replace in to_replace:
+            content = content.replace(this_to_replace[0], this_to_replace[1])
+        with open(file, "w", encoding='utf-8') as f:
+            f.write(content)
+
+
